148bbcbc62e97e317061673c997082906f2a4b1d
ozuma <ozuma5119@gmail.com>
2020-10-27 22:20:11 +0900

    fix: country val

--- openphish/main.sh
+++ openphish/main.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+target_country="Japan"
+
 rm *.txt
-curl -o url.txt https://www.openphish.com/feed.txt
-./url2FQDN.sh url.txt > fqdn.txt
+curl -O https://www.openphish.com/feed.txt
+head -n 2500 feed.txt | grep -v appspot.com > url.txt
+
+./url2FQDN.sh url.txt | sort | uniq > fqdn.txt
 ./fqdn2ip.sh fqdn.txt | tee host-ip.txt
 cut -d' ' -f2 host-ip.txt > ip.txt
 ./ip2country.sh ip.txt | tee result.txt
 echo "---------------------------------"
-grep Japan result.txt | sort | uniq
+grep $target_country result.txt | sort | uniq
 
